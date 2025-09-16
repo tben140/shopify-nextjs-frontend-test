ca006506dbf147c0ebabb3057f6d6aa2578f38f1
Ben Taylor <tben140@gmail.com>
2025-09-16 14:47:51 +0100

    Update next.config.ts to include allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,20 @@ export default {
   experimental: {
     ppr: true,
     inlineCss: true,
-    useCache: true
+    useCache: true,
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/s/files/**",
+      },
+    ],
+  },
+  allowedDevOrigins: [
+    "0050ae3c8300.ngrok-free.app",
+    "*.0050ae3c8300.ngrok-free.app",
+  ],
 };
